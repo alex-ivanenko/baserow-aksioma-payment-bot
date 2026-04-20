b6a57c1fa95617be6d7a46571cdefd13e7e548ce
Alex Ivanenko <alex.ivanenko0215@gmail.com>
2026-04-20 15:10:35 +0300

    Delete main.py

--- main.py
+++
@@ -1,8 +0,0 @@
-import asyncio
-from bot.main import main
-
-if __name__ == "__main__":
-    try:
-        asyncio.run(main())
-    except KeyboardInterrupt:
-        print("Бот остановлен вручную")
